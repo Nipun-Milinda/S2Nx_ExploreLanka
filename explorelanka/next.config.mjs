6c22867b521d0f5c04e08b2a3d706014fac15a5e
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-09-05 11:21:27 +0530

    added mui tel input

--- explorelanka/next.config.mjs
+++ explorelanka/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['sequelize'],
-      },
+  transpilePackages: ['mui-tel-input'],
+  experimental: {
+    serverComponentsExternalPackages: ['sequelize'],
+  },
 };
 
 export default nextConfig;
