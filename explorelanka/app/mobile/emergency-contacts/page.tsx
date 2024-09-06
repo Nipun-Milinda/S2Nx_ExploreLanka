2dc914ee5aa5bea5ebd39719b3d02e8dc30f630a
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-09-06 12:40:34 +0530

    EL | bug fixes - Emergency contacts page

--- explorelanka/app/mobile/emergency-contacts/page.tsx
+++ explorelanka/app/mobile/emergency-contacts/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Button, Checkbox } from '@material-tailwind/react';
 import MobileTextInput from '@/components/mobileTextInput';
 import { PlusIcon } from '@heroicons/react/solid';
@@ -9,15 +9,30 @@ import './EmergencyContacts.css';
 
 const EmergencyContacts = () => {
   const [contactName, setContactName] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-  const [primaryContact, setPrimaryContact] = useState<boolean>(false);
   const [countryCode, setCountryCode] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
   const [relationship, setRelationship] = useState<string>('');
   const [houseNumber, setHouseNumber] = useState<string>('');
   const [street, setStreet] = useState<string>('');
   const [city, setCity] = useState<string>('');
+  const [postalCode, setPostalCode] = useState<string>('');
   const [showSriLankaContact, setShowSriLankaContact] =
     useState<boolean>(false);
+
+  const sriLankaContactRef = useRef(null);
+
+  const logDetails = () => {
+    console.log({
+      contactName,
+      countryCode,
+      phoneNumber,
+      relationship,
+      houseNumber,
+      street,
+      city,
+      postalCode,
+    });
+  };
 
   return (
     <div className="flex flex-col min-h-screen justify-between">
@@ -25,12 +40,18 @@ const EmergencyContacts = () => {
         <p className="text-3xl font-semibold text-oceanBlue text-left">
           Emergency Contacts
         </p>
-
+        {/* own country section starts  */}
         <div className="w-full my-4">
           <p className="text-xl mb-1">Own Country</p>
           <div className="w-full my-2 bg-gray-200 p-4 rounded-lg">
             <div className="w-full my-2">
-              <MobileTextInput label="Name" />
+              <MobileTextInput
+                label="Name"
+                value={contactName}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setContactName(e.target.value)
+                }
+              />
             </div>
             <p className="text-xl mb-1">Mobile number</p>
 
@@ -44,21 +65,24 @@ const EmergencyContacts = () => {
                 <option value="+1">+1 (USA)</option>
                 <option value="+44">+44 (UK)</option>
                 <option value="+91">+91 (India)</option>
-                {/* Add more country codes as needed */}
               </select>
               <input
                 type="text"
                 className="w-2/3 p-2 border rounded"
                 placeholder="Telephone Number"
                 value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPhoneNumber(e.target.value)
+                }
               />
             </div>
             <div className="w-full my-2">
               <MobileTextInput
                 label="Relationship"
                 value={relationship}
-                onChange={(e) => setRelationship(e.target.value)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setRelationship(e.target.value)
+                }
               />
             </div>
             <div className="w-full my-4">
@@ -68,31 +92,50 @@ const EmergencyContacts = () => {
                   <MobileTextInput
                     label="House Number"
                     value={houseNumber}
-                    onChange={(e) => setHouseNumber(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setHouseNumber(e.target.value)
+                    }
                   />
                 </div>
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="Street"
                     value={street}
-                    onChange={(e) => setStreet(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setStreet(e.target.value)
+                    }
                   />
                 </div>
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="City"
                     value={city}
-                    onChange={(e) => setCity(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setCity(e.target.value)
+                    }
+                  />
+                </div>
+                <div className="w-full my-2">
+                  <MobileTextInput
+                    label="Postal code"
+                    value={postalCode}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setPostalCode(e.target.value)
+                    }
                   />
                 </div>
               </div>
             </div>
           </div>
         </div>
-
+        {/* own country section ends */}
         <div className="w-full my-2 flex items-center justify-end">
           <div className="border-4 border-[#094b59] rounded-full p-1">
-            <PlusIcon className="h-6 w-6 text-[#094b59] stroke-1" />
+            <PlusIcon
+              className="h-6 w-6 text-[#094b59] stroke-1"
+              id="firstPlusIcon"
+              onClick={logDetails}
+            />
           </div>
         </div>
 
@@ -109,64 +152,103 @@ const EmergencyContacts = () => {
           timeout={300}
           classNames="card-input"
           unmountOnExit
+          nodeRef={sriLankaContactRef}
         >
-          <div className="w-full my-2 bg-gray-200 p-4 rounded-lg">
-            <div className="w-full my-2">
-              <MobileTextInput label="Name" />
-            </div>
-            <p className="text-xl mb-1">Mobile number</p>
+          <div>
+            <div
+              ref={sriLankaContactRef}
+              className="w-full my-2 bg-gray-200 p-4 rounded-lg"
+            >
+              <div className="w-full my-2">
+                <MobileTextInput
+                  label="Name"
+                  value={contactName}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setContactName(e.target.value)
+                  }
+                />
+              </div>
+              <p className="text-xl mb-1">Mobile number</p>
 
-            <div className="w-full my-2 flex space-x-2">
-              <select
-                className="w-1/3 p-2 border rounded"
-                value={countryCode}
-                onChange={(e) => setCountryCode(e.target.value)}
-              >
-                <option value="">Select Country Code</option>
-                <option value="+1">+1 (USA)</option>
-                <option value="+44">+44 (UK)</option>
-                <option value="+91">+91 (India)</option>
-                {/* Add more country codes as needed */}
-              </select>
-              <input
-                type="text"
-                className="w-2/3 p-2 border rounded"
-                placeholder="Telephone Number"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
+              <div className="w-full my-2 flex space-x-2">
+                <select
+                  className="w-1/3 p-2 border rounded"
+                  value={countryCode}
+                  onChange={(e) => setCountryCode(e.target.value)}
+                >
+                  <option value="">Select Country Code</option>
+                  <option value="+1">+1 (USA)</option>
+                  <option value="+44">+44 (UK)</option>
+                  <option value="+91">+91 (India)</option>
+                  {/* Add more country codes as needed */}
+                </select>
+                <input
+                  type="text"
+                  className="w-2/3 p-2 border rounded"
+                  placeholder="Telephone Number"
+                  value={phoneNumber}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPhoneNumber(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-full my-2">
+                <MobileTextInput
+                  label="Relationship"
+                  value={relationship}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setRelationship(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-full my-4">
+                <p className="text-xl mb-1">Address of Employee</p>
+                <div className="w-full my-2 bg-gray-200 p-4 rounded-lg border border-gray-400">
+                  <div className="w-full my-2">
+                    <MobileTextInput
+                      label="House Number"
+                      value={houseNumber}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setHouseNumber(e.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="w-full my-2">
+                    <MobileTextInput
+                      label="Street"
+                      value={street}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setStreet(e.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="w-full my-2">
+                    <MobileTextInput
+                      label="City"
+                      value={city}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setCity(e.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="w-full my-2">
+                    <MobileTextInput
+                      label="Postal code"
+                      value={postalCode}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setPostalCode(e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="w-full my-2">
-              <MobileTextInput
-                label="Relationship"
-                value={relationship}
-                onChange={(e) => setRelationship(e.target.value)}
-              />
-            </div>
-            <div className="w-full my-4">
-              <p className="text-xl mb-1">Address of Employee</p>
-              <div className="w-full my-2 bg-gray-200 p-4 rounded-lg border border-gray-400">
-                <div className="w-full my-2">
-                  <MobileTextInput
-                    label="House Number"
-                    value={houseNumber}
-                    onChange={(e) => setHouseNumber(e.target.value)}
-                  />
-                </div>
-                <div className="w-full my-2">
-                  <MobileTextInput
-                    label="Street"
-                    value={street}
-                    onChange={(e) => setStreet(e.target.value)}
-                  />
-                </div>
-                <div className="w-full my-2">
-                  <MobileTextInput
-                    label="City"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                  />
-                </div>
+            <div className="w-full my-2 flex items-center justify-end">
+              <div className="border-4 border-[#094b59] rounded-full p-1">
+                <PlusIcon
+                  className="h-6 w-6 text-[#094b59] stroke-1"
+                  id="firstPlusIcon"
+                />
               </div>
             </div>
           </div>
@@ -174,7 +256,14 @@ const EmergencyContacts = () => {
       </div>
 
       <div className="w-full p-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-4">
-        <Button color="blue" fullWidth style={{ backgroundColor: '#094b59' }}>
+        <Button
+          color="blue"
+          fullWidth={true}
+          style={{ backgroundColor: '#094b59' }}
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
           Next
         </Button>
         <Button
@@ -185,6 +274,9 @@ const EmergencyContacts = () => {
             color: '#094b59',
             borderWidth: '3px',
           }}
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
         >
           Back
         </Button>
