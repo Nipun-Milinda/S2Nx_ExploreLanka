a6280091f4b48ef93010f0f4ca06c18baa7461db
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-09-06 20:15:33 +0530

    EL | bug fixes in emergency contacts

--- explorelanka/app/mobile/emergency-contacts/page.tsx
+++ explorelanka/app/mobile/emergency-contacts/page.tsx
@@ -3,35 +3,201 @@
 import React, { useState, useRef } from 'react';
 import { Button, Checkbox } from '@material-tailwind/react';
 import MobileTextInput from '@/components/mobileTextInput';
-import { PlusIcon } from '@heroicons/react/solid';
+import { PlusIcon, XIcon } from '@heroicons/react/solid';
 import { CSSTransition } from 'react-transition-group';
+import { message } from 'antd';
 import './EmergencyContacts.css';
 
 const EmergencyContacts = () => {
-  const [contactName, setContactName] = useState<string>('');
-  const [countryCode, setCountryCode] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-  const [relationship, setRelationship] = useState<string>('');
-  const [houseNumber, setHouseNumber] = useState<string>('');
-  const [street, setStreet] = useState<string>('');
-  const [city, setCity] = useState<string>('');
-  const [postalCode, setPostalCode] = useState<string>('');
+  const [ownContactName, setOwnContactName] = useState<string>('');
+  const [ownCountryCode, setOwnCountryCode] = useState<string>('');
+  const [ownPhoneNumber, setOwnPhoneNumber] = useState<string>('');
+  const [ownRelationship, setOwnRelationship] = useState<string>('');
+  const [ownHouseNumber, setOwnHouseNumber] = useState<string>('');
+  const [ownStreet, setOwnStreet] = useState<string>('');
+  const [ownCity, setOwnCity] = useState<string>('');
+  const [ownPostalCode, setOwnPostalCode] = useState<string>('');
+
+  const [sriLankaContactName, setSriLankaContactName] = useState<string>('');
+  const [sriLankaCountryCode, setSriLankaCountryCode] = useState<string>('');
+  const [sriLankaPhoneNumber, setSriLankaPhoneNumber] = useState<string>('');
+  const [sriLankaRelationship, setSriLankaRelationship] = useState<string>('');
+  const [sriLankaHouseNumber, setSriLankaHouseNumber] = useState<string>('');
+  const [sriLankaStreet, setSriLankaStreet] = useState<string>('');
+  const [sriLankaCity, setSriLankaCity] = useState<string>('');
+  const [sriLankaPostalCode, setSriLankaPostalCode] = useState<string>('');
+
   const [showSriLankaContact, setShowSriLankaContact] =
     useState<boolean>(false);
+  const [ownDetailsArray, setOwnDetailsArray] = useState<any[]>([]);
+  const [sriLankaDetailsArray, setSriLankaDetailsArray] = useState<any[]>([]);
 
   const sriLankaContactRef = useRef(null);
 
-  const logDetails = () => {
-    console.log({
-      contactName,
-      countryCode,
-      phoneNumber,
-      relationship,
-      houseNumber,
-      street,
-      city,
-      postalCode,
-    });
+  const validateDetails = (isOwnCountry: boolean) => {
+    if (isOwnCountry) {
+      if (
+        !ownContactName ||
+        !ownCountryCode ||
+        !ownPhoneNumber ||
+        !ownRelationship ||
+        !ownHouseNumber ||
+        !ownStreet ||
+        !ownCity ||
+        !ownPostalCode
+      ) {
+        message.error('Please fill in all the fields.');
+        return false;
+      }
+
+      const isDuplicateName = ownDetailsArray.some(
+        (detail) => detail.contactName === ownContactName
+      );
+
+      if (isDuplicateName) {
+        message.error('A contact with this name already exists.');
+        return false;
+      }
+
+      const isDuplicatePhone = ownDetailsArray.some(
+        (detail) => detail.phoneNumber === ownPhoneNumber
+      );
+
+      if (isDuplicatePhone) {
+        message.error('A contact with this phone number already exists.');
+        return false;
+      }
+    } else {
+      if (
+        !sriLankaContactName ||
+        !sriLankaCountryCode ||
+        !sriLankaPhoneNumber ||
+        !sriLankaRelationship ||
+        !sriLankaHouseNumber ||
+        !sriLankaStreet ||
+        !sriLankaCity ||
+        !sriLankaPostalCode
+      ) {
+        message.error('Please fill in all the fields.');
+        return false;
+      }
+
+      const isDuplicateName = sriLankaDetailsArray.some(
+        (detail) => detail.contactName === sriLankaContactName
+      );
+
+      if (isDuplicateName) {
+        message.error('A contact with this name already exists.');
+        return false;
+      }
+
+      const isDuplicatePhone = sriLankaDetailsArray.some(
+        (detail) => detail.phoneNumber === sriLankaPhoneNumber
+      );
+
+      if (isDuplicatePhone) {
+        message.error('A contact with this phone number already exists.');
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  const logDetails = (isOwnCountry: boolean) => {
+    if (!validateDetails(isOwnCountry)) return;
+
+    if (isOwnCountry) {
+      const newDetails = {
+        contactName: ownContactName,
+        countryCode: ownCountryCode,
+        phoneNumber: ownPhoneNumber,
+        relationship: ownRelationship,
+        houseNumber: ownHouseNumber,
+        street: ownStreet,
+        city: ownCity,
+        postalCode: ownPostalCode,
+      };
+
+      console.log(newDetails);
+      setOwnDetailsArray((prevDetails) => [...prevDetails, newDetails]);
+
+      // Clear all input fields
+      clearFields(true);
+    } else {
+      const newDetails = {
+        contactName: sriLankaContactName,
+        countryCode: sriLankaCountryCode,
+        phoneNumber: sriLankaPhoneNumber,
+        relationship: sriLankaRelationship,
+        houseNumber: sriLankaHouseNumber,
+        street: sriLankaStreet,
+        city: sriLankaCity,
+        postalCode: sriLankaPostalCode,
+      };
+
+      console.log(newDetails);
+      setSriLankaDetailsArray((prevDetails) => [...prevDetails, newDetails]);
+
+      // Clear all input fields
+      clearFields(false);
+    }
+  };
+
+  const clearFields = (isOwnCountry: boolean) => {
+    if (isOwnCountry) {
+      setOwnContactName('');
+      setOwnCountryCode('');
+      setOwnPhoneNumber('');
+      setOwnRelationship('');
+      setOwnHouseNumber('');
+      setOwnStreet('');
+      setOwnCity('');
+      setOwnPostalCode('');
+    } else {
+      setSriLankaContactName('');
+      setSriLankaCountryCode('');
+      setSriLankaPhoneNumber('');
+      setSriLankaRelationship('');
+      setSriLankaHouseNumber('');
+      setSriLankaStreet('');
+      setSriLankaCity('');
+      setSriLankaPostalCode('');
+    }
+  };
+
+  const fillDetails = (details: any, isOwnCountry: boolean) => {
+    if (isOwnCountry) {
+      setOwnContactName(details.contactName);
+      setOwnCountryCode(details.countryCode);
+      setOwnPhoneNumber(details.phoneNumber);
+      setOwnRelationship(details.relationship);
+      setOwnHouseNumber(details.houseNumber);
+      setOwnStreet(details.street);
+      setOwnCity(details.city);
+      setOwnPostalCode(details.postalCode);
+    } else {
+      setSriLankaContactName(details.contactName);
+      setSriLankaCountryCode(details.countryCode);
+      setSriLankaPhoneNumber(details.phoneNumber);
+      setSriLankaRelationship(details.relationship);
+      setSriLankaHouseNumber(details.houseNumber);
+      setSriLankaStreet(details.street);
+      setSriLankaCity(details.city);
+      setSriLankaPostalCode(details.postalCode);
+    }
+  };
+
+  const removeDetail = (index: number, isOwnCountry: boolean) => {
+    if (isOwnCountry) {
+      setOwnDetailsArray((prevDetails) =>
+        prevDetails.filter((_, i) => i !== index)
+      );
+    } else {
+      setSriLankaDetailsArray((prevDetails) =>
+        prevDetails.filter((_, i) => i !== index)
+      );
+    }
   };
 
   return (
@@ -40,6 +206,26 @@ const EmergencyContacts = () => {
         <p className="text-3xl font-semibold text-oceanBlue text-left">
           Emergency Contacts
         </p>
+        {/* own details -contact cards */}
+        <div className="flex flex-wrap">
+          {ownDetailsArray.map((detail, index) => (
+            <div
+              key={index}
+              className="w-full md:w-1/2 lg:w-1/3 my-2 p-4 rounded-lg border border-gray-400 cursor-pointer relative lg:mr-4"
+              style={{ backgroundColor: '#d1f7f5' }}
+              onClick={() => fillDetails(detail, true)}
+            >
+              <p className="text-xl mb-1">{detail.contactName}</p>
+              <XIcon
+                className="h-6 w-6 text-[#094b59] stroke-1 cursor-pointer absolute top-1/2 right-2 transform -translate-y-1/2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeDetail(index, true);
+                }}
+              />
+            </div>
+          ))}
+        </div>
         {/* own country section starts  */}
         <div className="w-full my-4">
           <p className="text-xl mb-1">Own Country</p>
@@ -47,9 +233,9 @@ const EmergencyContacts = () => {
             <div className="w-full my-2">
               <MobileTextInput
                 label="Name"
-                value={contactName}
+                value={ownContactName}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setContactName(e.target.value)
+                  setOwnContactName(e.target.value)
                 }
               />
             </div>
@@ -58,8 +244,8 @@ const EmergencyContacts = () => {
             <div className="w-full my-2 flex space-x-2">
               <select
                 className="w-1/3 p-2 border rounded"
-                value={countryCode}
-                onChange={(e) => setCountryCode(e.target.value)}
+                value={ownCountryCode}
+                onChange={(e) => setOwnCountryCode(e.target.value)}
               >
                 <option value="">Select Country Code</option>
                 <option value="+1">+1 (USA)</option>
@@ -70,18 +256,18 @@ const EmergencyContacts = () => {
                 type="text"
                 className="w-2/3 p-2 border rounded"
                 placeholder="Telephone Number"
-                value={phoneNumber}
+                value={ownPhoneNumber}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setPhoneNumber(e.target.value)
+                  setOwnPhoneNumber(e.target.value)
                 }
               />
             </div>
             <div className="w-full my-2">
               <MobileTextInput
                 label="Relationship"
-                value={relationship}
+                value={ownRelationship}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setRelationship(e.target.value)
+                  setOwnRelationship(e.target.value)
                 }
               />
             </div>
@@ -91,36 +277,36 @@ const EmergencyContacts = () => {
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="House Number"
-                    value={houseNumber}
+                    value={ownHouseNumber}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setHouseNumber(e.target.value)
+                      setOwnHouseNumber(e.target.value)
                     }
                   />
                 </div>
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="Street"
-                    value={street}
+                    value={ownStreet}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setStreet(e.target.value)
+                      setOwnStreet(e.target.value)
                     }
                   />
                 </div>
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="City"
-                    value={city}
+                    value={ownCity}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setCity(e.target.value)
+                      setOwnCity(e.target.value)
                     }
                   />
                 </div>
                 <div className="w-full my-2">
                   <MobileTextInput
                     label="Postal code"
-                    value={postalCode}
+                    value={ownPostalCode}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setPostalCode(e.target.value)
+                      setOwnPostalCode(e.target.value)
                     }
                   />
                 </div>
@@ -129,12 +315,19 @@ const EmergencyContacts = () => {
           </div>
         </div>
         {/* own country section ends */}
-        <div className="w-full my-2 flex items-center justify-end">
+        <div className="w-full my-2 flex items-center justify-end space-x-2">
+          <div className="border-4 border-[#094b59] rounded-full p-1">
+            <XIcon
+              className="h-6 w-6 text-[#094b59] stroke-1 cursor-pointer"
+              id="clearIcon"
+              onClick={() => clearFields(true)}
+            />
+          </div>
           <div className="border-4 border-[#094b59] rounded-full p-1">
             <PlusIcon
-              className="h-6 w-6 text-[#094b59] stroke-1"
+              className="h-6 w-6 text-[#094b59] stroke-1 cursor-pointer"
               id="firstPlusIcon"
-              onClick={logDetails}
+              onClick={() => logDetails(true)}
             />
           </div>
         </div>
@@ -147,6 +340,7 @@ const EmergencyContacts = () => {
           />
         </div>
 
+        {/* This country section start */}
         <CSSTransition
           in={showSriLankaContact}
           timeout={300}
@@ -155,6 +349,26 @@ const EmergencyContacts = () => {
           nodeRef={sriLankaContactRef}
         >
           <div>
+            <div className="flex flex-wrap">
+              {/* this country details contact cards */}
+              {sriLankaDetailsArray.map((detail, index) => (
+                <div
+                  key={index}
+                  className="w-full md:w-1/2 lg:w-1/3 my-2 p-4 rounded-lg border border-gray-400 cursor-pointer relative lg:mr-4"
+                  style={{ backgroundColor: '#d1f7f5' }}
+                  onClick={() => fillDetails(detail, false)}
+                >
+                  <p className="text-xl mb-1">{detail.contactName}</p>
+                  <XIcon
+                    className="h-6 w-6 text-[#094b59] stroke-1 cursor-pointer absolute top-1/2 right-2 transform -translate-y-1/2"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      removeDetail(index, false);
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
             <div
               ref={sriLankaContactRef}
               className="w-full my-2 bg-gray-200 p-4 rounded-lg"
@@ -162,9 +376,9 @@ const EmergencyContacts = () => {
               <div className="w-full my-2">
                 <MobileTextInput
                   label="Name"
-                  value={contactName}
+                  value={sriLankaContactName}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setContactName(e.target.value)
+                    setSriLankaContactName(e.target.value)
                   }
                 />
               </div>
@@ -173,8 +387,8 @@ const EmergencyContacts = () => {
               <div className="w-full my-2 flex space-x-2">
                 <select
                   className="w-1/3 p-2 border rounded"
-                  value={countryCode}
-                  onChange={(e) => setCountryCode(e.target.value)}
+                  value={sriLankaCountryCode}
+                  onChange={(e) => setSriLankaCountryCode(e.target.value)}
                 >
                   <option value="">Select Country Code</option>
                   <option value="+1">+1 (USA)</option>
@@ -186,18 +400,18 @@ const EmergencyContacts = () => {
                   type="text"
                   className="w-2/3 p-2 border rounded"
                   placeholder="Telephone Number"
-                  value={phoneNumber}
+                  value={sriLankaPhoneNumber}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setPhoneNumber(e.target.value)
+                    setSriLankaPhoneNumber(e.target.value)
                   }
                 />
               </div>
               <div className="w-full my-2">
                 <MobileTextInput
                   label="Relationship"
-                  value={relationship}
+                  value={sriLankaRelationship}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setRelationship(e.target.value)
+                    setSriLankaRelationship(e.target.value)
                   }
                 />
               </div>
@@ -207,36 +421,36 @@ const EmergencyContacts = () => {
                   <div className="w-full my-2">
                     <MobileTextInput
                       label="House Number"
-                      value={houseNumber}
+                      value={sriLankaHouseNumber}
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setHouseNumber(e.target.value)
+                        setSriLankaHouseNumber(e.target.value)
                       }
                     />
                   </div>
                   <div className="w-full my-2">
                     <MobileTextInput
                       label="Street"
-                      value={street}
+                      value={sriLankaStreet}
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setStreet(e.target.value)
+                        setSriLankaStreet(e.target.value)
                       }
                     />
                   </div>
                   <div className="w-full my-2">
                     <MobileTextInput
                       label="City"
-                      value={city}
+                      value={sriLankaCity}
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setCity(e.target.value)
+                        setSriLankaCity(e.target.value)
                       }
                     />
                   </div>
                   <div className="w-full my-2">
                     <MobileTextInput
                       label="Postal code"
-                      value={postalCode}
+                      value={sriLankaPostalCode}
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setPostalCode(e.target.value)
+                        setSriLankaPostalCode(e.target.value)
                       }
                     />
                   </div>
@@ -246,13 +460,15 @@ const EmergencyContacts = () => {
             <div className="w-full my-2 flex items-center justify-end">
               <div className="border-4 border-[#094b59] rounded-full p-1">
                 <PlusIcon
-                  className="h-6 w-6 text-[#094b59] stroke-1"
-                  id="firstPlusIcon"
+                  className="h-6 w-6 text-[#094b59] stroke-1 cursor-pointer"
+                  id="secondPlusIcon"
+                  onClick={() => logDetails(false)}
                 />
               </div>
             </div>
           </div>
         </CSSTransition>
+        {/* This country section ends */}
       </div>
 
       <div className="w-full p-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-4">
--- explorelanka/components/mobileTextInput.tsx
+++ explorelanka/components/mobileTextInput.tsx
@@ -1,24 +1,26 @@
-"use client";
+'use client';
 
 import React from 'react';
-import { Input } from "@material-tailwind/react";
+import { Input } from '@material-tailwind/react';
 
 interface MobileTextInputProps {
   label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const MobileTextInput: React.FC<MobileTextInputProps> = (props) => {
-  // Implement your component logic here
-
   return (
     <div className="w-full my-4">
-        <p className='text-xl mb-1'>{props.label}</p>
-        <Input
-          containerProps={{ className:'bg-black/10 rounded-[7px]'}}
-          labelProps={{
-            className: "before:mr-0 after:ml-0",
-          }}
-          />
+      <p className="text-xl mb-1">{props.label}</p>
+      <Input
+        containerProps={{ className: 'bg-black/10 rounded-[7px]' }}
+        labelProps={{
+          className: 'before:mr-0 after:ml-0',
+        }}
+        value={props.value}
+        onChange={props.onChange}
+      />
     </div>
   );
 };
